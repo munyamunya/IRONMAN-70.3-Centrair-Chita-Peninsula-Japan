d86637615ec7b479642fe8386121191169e9f43a
munyamunya <shuuji.miyoshi@gmail.com>
2020-10-21 22:24:00 +0900

    Add files via upload

---
+++ raceresult-loader.py
@@ -0,0 +1,13 @@
+# Base URL https://systemway.jp/20ironman
+
+mem_urllist = [
+    "https://systemway.jp/20ironman?p=1&di=4&sk=race_num&sv=0", 
+    "https://systemway.jp/20ironman?p=2&di=4&sk=race_num&sv=0", 
+    "https://systemway.jp/20ironman?p=3&di=4&sk=race_num&sv=0", 
+    "https://systemway.jp/20ironman?p=4&di=4&sk=race_num&sv=0", 
+    "https://systemway.jp/20ironman?p=5&di=4&sk=race_num&sv=0",     
+    ]
+
+# https://note.nkmk.me/python-pandas-web-html-table-scraping/
+
+import pandas as pd
